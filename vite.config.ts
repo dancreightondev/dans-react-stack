8d9ee9abb515854d6862917b5c3f79c21ce42e25
dancreightondev <22957063+dancreightondev@users.noreply.github.com>
2024-10-01 11:40:38 +0100

    update vite config to resolve import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    css: {
-        postcss: {
-            plugins: [tailwindcss()],
-        },
-    },
-});
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()]
+    }
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
+})
